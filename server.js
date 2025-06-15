387270d19a19ee4eb1e9a06b198a0dddc39eb0cc
blueshadow97 <sanu71169@gmail.com>
2025-06-15 14:42:36 -0400

    update

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-const express = require("express");
+const express = require('express');
+const addonInterface = require('./index'); // your index.js must export the addon
 const app = express();
-const addonInterface = require("./index");
 
-app.get("/manifest.json", (req, res) => {
-  res.send(addonInterface.manifest);
+app.get('/manifest.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(addonInterface.manifest));
 });
 
-app.get("/:resource/:type/:id.json", (req, res) => {
-  addonInterface.get(req, res);
+app.get('/:resource/:type/:id?.json', (req, res) => {
+  addonInterface.get(req.params).then(resp => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(resp));
+  }).catch(err => {
+    res.statusCode = 500;
+    res.end(JSON.stringify({ error: err.message }));
+  });
 });
 
-const PORT = process.env.PORT || 7000;
-app.listen(PORT, () => {
-  console.log("PelisenHD Addon running at http://localhost:" + PORT);
-});
+module.exports = app;
